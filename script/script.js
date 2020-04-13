ca5a2809a7f61a60a77e99517b24683b19ac8e15
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-04-13 19:46:23 +0300

    fixed: измененить разметку страницы. поправить код

--- script/script.js
+++ script/script.js
@@ -3,36 +3,39 @@ const editButton = content.querySelector('.profile__button-edit');
 const resetButton = content.querySelector('.popup-container__button-reset');
 let popup = content.querySelector('.popup');
 let addButton = content.querySelector('.popup-container__button-add');
+let formElement = content.querySelector('.popup-container');
 
 function editForm () {
   popup.classList.add('popup_opened');
+  // выбираем поля формы
+  let nameInput = content.querySelector('.popup-container__infoForm_author');
+  let jobInput = content.querySelector('.popup-container__infoForm_aboutYourself');
+  // присваиваем им значения
+  nameInput.value;
+  jobInput.value;
 }
 
 function resetForm () {
   popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', editForm);
-resetButton.addEventListener('click', resetForm);
-
-let formElement = content.querySelector('.popup-container');
-
 function formSubmitHandler (evt) {
     evt.preventDefault();
-
-    let nameInput = content.querySelector('.popup-container__author');
-    let jobInput = content.querySelector('.popup-container__aboutYourself');
-
-    nameInput.getAttribute('value');
-    jobInput.getAttribute('value');
-
+    // выбираем поля формы
+    let nameInput = content.querySelector('.popup-container__infoForm_author');
+    let jobInput = content.querySelector('.popup-container__infoForm_aboutYourself');
+    // присваиваем им значения
+    nameInput.value;
+    jobInput.value;
+    // выбираем элементы, которые необходимо изменить
     let profileAuthor = content.querySelector('.profile__author');
     let profileSpecialty = content.querySelector('.profile__specialty');
-
+    // присваиваем им новые значения
     profileAuthor.textContent = nameInput.value;
     profileSpecialty.textContent = jobInput.value;
-
-    addButton.addEventListener('click', resetForm);
 }
 
+editButton.addEventListener('click', editForm);
+resetButton.addEventListener('click', resetForm);
 formElement.addEventListener('submit', formSubmitHandler);
+addButton.addEventListener('click', resetForm);
