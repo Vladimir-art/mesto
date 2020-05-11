288f11f3552c761d3e99e491df5b91be71d15622
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-05-11 12:11:08 +0300

    Добавить валидацию формам

--- script/validate.js
+++ script/validate.js
@@ -1,28 +1,47 @@
-const formElement = document.querySelector('.popup-container');
-const formInputAuthor = formElement.querySelector('.popup-container__infoform_author');
-const formInputJob = formElement.querySelector('.popup-container__infoform_aboutyourself');
 
-const showInputError = (element) => {
+const showInputError = (formElement, element, errorMessage) => {
+  const errorElement = formElement.querySelector(`#${element.id}-error`);
   element.classList.add('popup-container__infoform_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup-container__input-error_active');
 };
 
-const hideInputError = (element) => {
+const hideInputError = (formElement, element) => {
+  const errorElement = formElement.querySelector(`#${element.id}-error`);
   element.classList.remove('popup-container__infoform_type_error');
+  errorElement.classList.remove('popup-container__input-error_active');
+  errorElement.textContent = '';
 };
 
-const isValid = () => {
-  if (!formInputAuthor.validity.valid) {
+const isValid = (formElement, formInput) => {
+  if (!formInput.validity.valid) {
     // Если поле не проходит валидацию, покажем ошибку
-    showInputError(formInputAuthor);
+    showInputError(formElement, formInput, formInput.validationMessage);
   } else {
     // Если проходит, скроем
-    hideInputError(formInputAuthor);
+    hideInputError(formElement, formInput);
   }
 };
 
-formElement.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-})
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup-container__infoform'));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', ()=> {
+      isValid(formElement, inputElement);
+    });
+  })
+}
 
-formInputAuthor.addEventListener('input', isValid);
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup-container'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    })
+    setEventListeners(formElement);
+  })
+}
+
+enableValidation();
+
 
