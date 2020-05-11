4332f1bc8eea0c6b2feef3e12b2a176c8e282319
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-05-11 14:02:11 +0300

    Добавить объект

--- script/validate.js
+++ script/validate.js
@@ -25,12 +25,15 @@ const isValid = (formElement, formInput) => {
 
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.popup-container__infoform'));
+  const buttonElement = formElement.querySelector('.popup-container__button-add');
+  toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', ()=> {
       isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
     });
   })
-}
+};
 
 const enableValidation = () => {
   const formList = Array.from(document.querySelectorAll('.popup-container'));
@@ -40,8 +43,33 @@ const enableValidation = () => {
     })
     setEventListeners(formElement);
   })
-}
+};
 
-enableValidation();
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+      // Если поле не валидно, колбэк вернёт true
+  // Обход массива прекратится и вся фунцкция
+  // hasInvalidInput вернёт true
 
+  return !inputElement.validity.valid;
+  })
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup-container__button-add_error');
+  } else {
+        // иначе сделай кнопку активной
+    buttonElement.classList.remove('popup-container__button-add_error');
+  }
+};
+
+enableValidation({
+  formElement: '.popup-container',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+});
 
