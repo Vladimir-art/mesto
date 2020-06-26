7da76f071489c98f7990d8ec4745ec6bd6f56f34
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-06-27 00:02:22 +0300

    create 5 task

--- src/script/components/Card.js
+++ src/script/components/Card.js
@@ -1,10 +1,15 @@
+import { baseUrl } from "../utils/constants.js";
+import { API } from "./API.js";
+
 //функция создания карточки
 export class Card {
   constructor(item, {handleCardClick}, cardSelector) {
+    this._likes = item.likes;
     this._name = item.name; //имя картинки
     this._link = item.link; //ссылка на картинку
     this._handleCardClick = handleCardClick; //функция по открытию попапа с картинкой
     this._cardSelector = document.querySelector(cardSelector);
+    this._api = new API( {baseUrl} );
   }
   //находим template элемент на странице и клонируем их
   _getTemplate() {
@@ -20,11 +25,12 @@ export class Card {
     this._setEventListeners(); //добавляет карточке слушатели событий
     this._element.querySelector('.element__place').textContent = this._name; //присваивает значения
     this._element.querySelector('.element__image').setAttribute('src', this._link);
+    this._element.querySelector('.element__count').textContent = this._likes.length;
     return this._element; //возвращает карточку
   }
   //приватный метод: устанавливает слушатели событий
   _setEventListeners() {
-    this._element.querySelector('.element__button').addEventListener('click', () => {
+    this._element.querySelector('.element__button').addEventListener('click', (evt) => {
       this._handleLikeButton(); //ставить лайки карточкам
     });
     this._element.querySelector('.element__trash').addEventListener('click', () => {
@@ -32,7 +38,7 @@ export class Card {
     });
     this._element.querySelector('.element__image').addEventListener('click', () => {
       this._handleCardClick(this._name, this._link); //открывает попап с картинкой
-    })
+    });
   }
   //приватный метод: активный/неактивный лайк
   _handleLikeButton() {
